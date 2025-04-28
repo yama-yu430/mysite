6797e3f9c0659bd00bc22689e232c769d1bfcdee
yama-yu430 <worldpeace643@gmail.com>
2025-04-28 16:01:49 +0900

    回答を追加

--- src/curriculum2/Question5.java
+++ src/curriculum2/Question5.java
@@ -3,5 +3,7 @@ package curriculum2;
 public class Question5 {
     public static void main(String[] args){
         System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
     }
 }
