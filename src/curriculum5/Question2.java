aeeb14d821c4823b20c36a3893030ae8e8283897
yama-yu430 <worldpeace643@gmail.com>
2025-05-04 13:11:54 +0900

    回答を追加

---
+++ src/curriculum5/Question2.java
@@ -0,0 +1,7 @@
+package curriculum5;
+
+public class Question2 {
+    public static void main(String[] args){
+        System.out.println("4");
+    }
+}
---
+++ src/curriculum5/Question4.java
@@ -0,0 +1,8 @@
+package curriculum5;
+
+public class Question4 {
+    public static void main(String[] args){
+        int[][] num = {{8,4,7},{6,9,5}};
+        System.out.println(num[1][1]);
+    }
+}
